3c19af0aaeefefcbbeed3dfd7966c237711757f5
Victor Miti <victormiti@umusebo.com>
2016-11-19 15:50:17 +0200

    Changed a few Landing Page Items

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en"><head>
   <meta charset="utf-8">
-  <title>Snow Landing Page Theme | Created By Visual Soldiers</title>
+  <title>Agora Code Community | Lusaka, Zambia</title>
   <meta name="keywords" content="">
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  
+
   <meta property="og:title" content="">
 	<meta property="og:type" content="website">
 	<meta property="og:url" content="">
@@ -16,18 +16,18 @@
   <link rel="stylesheet" href="css/font-awesome.min.css">
   <link rel="stylesheet" href="css/animate.css">
   <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900|Montserrat:400,700' rel='stylesheet' type='text/css'>
-  
+
 
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/main.css">
 
   <script src="js/modernizr-2.7.1.js"></script>
-  
+
 </head>
 
 <body>
 
-    
+
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
@@ -36,59 +36,63 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="logo" href="index.html"><img src="img/logo.svg" alt="Logo"></a>
+          <a class="logo" href="index.html"><img src="img/agora_logo_alt.png" alt="Logo"></a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#pricing" class="scroll">Pricing</a></li>
+            <li><a href="#about" class="scroll">About</a></li>
+            <li><a href="#jobs" class="scroll">Jobs</a></li> <li><a href="#forums" class="scroll">Forums</a></li>
             <li><a href="#modal-form" data-toggle="modal">Sign in</a></li>
           </ul>
         </div><!--/.navbar-collapse -->
       </div>
     </div>
-        
+
     <header>
       <div class="container">
         <div class="row">
           <div class="col-xs-6">
-            <a href="index.html"><img src="img/logo.svg" alt="Logo"></a>
+            <a href="index.html"><img src="img/agora_logo.png" alt="Logo"></a>
           </div>
           <div class="col-xs-6 signin text-right navbar-nav">
-            <a href="#pricing" class="scroll">Pricing</a>&nbsp; &nbsp;<a href="#modal-form" data-toggle="modal">Sign in</a>
+            <a href="#about" class="scroll">About</a> &nbsp;&nbsp;
+            <a href="#forums" class="scroll">Forums</a> &nbsp;&nbsp;
+            <a href="#jobs" class="scroll">Jobs</a> &nbsp;&nbsp;
+            <a href="#modal-form" data-toggle="modal">Sign in</a>
           </div>
         </div>
-        
+
         <div class="row header-info">
           <div class="col-sm-10 col-sm-offset-1 text-center">
-            <h1 class="wow fadeIn">A free landing page theme</h1>
+            <h1 class="wow fadeIn">My Name is Brenda</h1>
             <br />
             <p class="lead wow fadeIn" data-wow-delay="0.5s">Lorem ipsum dolor sit amet, consectetur adipis. <br>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit sit dol mi arlna.</p>
             <br />
-              
+
             <div class="row">
               <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
                 <div class="row">
                   <div class="col-xs-6 text-right wow fadeInUp" data-wow-delay="1s">
-                    <a href="#be-the-first" class="btn btn-secondary btn-lg scroll">Learn More</a>
+                    <a href="#about" class="btn btn-secondary btn-lg scroll">Learn More</a>
                   </div>
                   <div class="col-xs-6 text-left wow fadeInUp" data-wow-delay="1.4s">
                     <a href="#invite" class="btn btn-primary btn-lg scroll">Request Invite</a>
                   </div>
-                </div><!--End Button Row-->  
+                </div><!--End Button Row-->
               </div>
             </div>
-            
+
           </div>
         </div>
       </div>
     </header>
-    
+
     <div class="mouse-icon hidden-xs">
 				<div class="scroll"></div>
 			</div>
-    
-    <section id="be-the-first" class="pad-xl">
+
+    <section id="about" class="pad-xl">
       <div class="container">
         <div class="row">
           <div class="col-sm-8 col-sm-offset-2 text-center margin-30 wow fadeIn" data-wow-delay="0.6s">
@@ -96,13 +100,13 @@
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipis.</p>
           </div>
         </div>
-        
+
         <div class="iphone wow fadeInUp" data-wow-delay="1s">
 	        <img src="img/iphone.png">
         </div>
       </div>
     </section>
-    
+
     <section id="main-info" class="pad-xl">
 	    <div class="container">
 		    <div class="row">
@@ -124,8 +128,8 @@
 		    </div>
 	    </div>
     </section>
-        
-        
+
+
     <!--Pricing-->
     <section id="pricing" class="pad-lg">
       <div class="container">
@@ -135,9 +139,9 @@
             <p class="white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.</p>
           </div>
         </div>
-        
+
         <div class="row margin-50">
-          
+
           <div class="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="1s">
             <br />
             <ul class="list-unstyled pricing-table text-center">
@@ -150,7 +154,7 @@
     					<li class="features last btn btn-secondary btn-wide"><a href="#">Get Started</a></li>
     				</ul>
           </div>
-          
+
           <div class="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="0.4s">
             <ul class="list-unstyled pricing-table active text-center">
     					<li class="headline"><h5 class="white">Professional</h5></li>
@@ -163,7 +167,7 @@
     					<li class="features last btn btn-secondary btn-wide"><a href="#">Get Started</a></li>
     				</ul>
           </div>
-          
+
           <div class="col-sm-4 pricing-container wow fadeInUp" data-wow-delay="1.3s">
             <br />
             <ul class="list-unstyled pricing-table text-center">
@@ -176,13 +180,13 @@
     					<li class="features last btn btn-secondary btn-wide"><a href="#">Get Started</a></li>
     				</ul>
           </div>
-          
+
         </div>
-        
+
       </div>
     </section>
-    
-    
+
+
     <section id="invite" class="pad-lg light-gray-bg">
       <div class="container">
         <div class="row">
@@ -192,7 +196,7 @@
             <br />
             <p class="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.</p>
             <br />
-            
+
             <div class="row">
               <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <form role="form">
@@ -208,38 +212,38 @@
         </div>
       </div>
     </section>
-    
-    
+
+
     <section id="press" class="pad-sm">
       <div class="container">
-        
+
         <div class="row margin-30 news-container">
           <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 wow fadeInLeft" data-wow-delay="0.8s">
             <a href="#" target="_blank">
             <img class="news-img pull-left" src="img/press-01.jpg" alt="Tech Crunch">
-            <p class="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.<br /> 
+            <p class="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut.<br />
             <small><em>Tech Crunch - January 15, 2015</em></small></p>
             </a>
           </div>
         </div>
-        
+
         <div class="row margin-30 news-container">
           <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 wow fadeInLeft" data-wow-delay="1.2s">
             <a href="#" target="_blank">
             <img class="news-img pull-left" src="img/press-02.jpg" alt="Forbes">
-            <p class="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut. <br /> 
+            <p class="black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra orci ut. <br />
             <small><em>Forbes - Feb 25, 2015</em></small></p>
             </a>
           </div>
         </div>
-        
+
       </div>
     </section>
-    
-    
+
+
     <footer>
       <div class="container">
-        
+
         <div class="row">
           <div class="col-sm-8 margin-20">
             <ul class="list-inline social">
@@ -249,13 +253,13 @@
               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
             </ul>
           </div>
-          
+
           <div class="col-sm-4 text-right">
             <p><small>Copyright &copy; 2014. All rights reserved. <br>
 	            Created by <a href="http://visualsoldiers.com">Visual Soldiers</a></small></p>
           </div>
         </div>
-        
+
       </div>
 
     </footer>
@@ -281,7 +285,7 @@
                   <label>
                     <input type="checkbox"> Remember me
                   </label>
-                </div>                
+                </div>
               </form>
             </div>
             <div class="col-sm-6">
@@ -292,13 +296,13 @@
               <a href="#" class="btn btn-info btn-block m-b-sm"><i class="fa fa-twitter pull-left"></i>Sign in with Twitter</a>
               <a href="#" class="btn btn-danger btn-block"><i class="fa fa-google-plus pull-left"></i>Sign in with Google+</a>
             </div>
-          </div>          
+          </div>
         </div>
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
   </div>
-    
-    
+
+
     <!-- Javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
@@ -312,7 +316,7 @@
   <!-- Bootstrap -->
   <script src="js/bootstrap.js"></script>
   <!-- App -->
-  <script src="js/app.js"></script>  
+  <script src="js/app.js"></script>
   <script src="js/slimscroll/jquery.slimscroll.min.js"></script>
     <div class="modal fade" id="modal-form">
     <div class="modal-dialog">
@@ -336,7 +340,7 @@
                   <label>
                     <input type="checkbox"> Remember me
                   </label>
-                </div>                
+                </div>
               </form>
             </div>
             <div class="col-sm-6">
@@ -347,7 +351,7 @@
               <a href="#" class="btn btn-info btn-block m-b-sm"><i class="fa fa-twitter pull-left"></i>Sign in with Twitter</a>
               <a href="#" class="btn btn-danger btn-block"><i class="fa fa-google-plus pull-left"></i>Sign in with Google+</a>
             </div>
-          </div>          
+          </div>
         </div>
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
@@ -356,7 +360,7 @@
   <script src="js/datepicker/bootstrap-datepicker.js"></script>
   <!-- slider -->
   <script src="js/slider/bootstrap-slider.js"></script>
-  <!-- file input --> 
+  <!-- file input -->
   <script src="js/file-input/bootstrap-filestyle.min.js"></script>
   <!-- wysiwyg -->
   <script src="js/wysiwyg/jquery.hotkeys.js"></script>
@@ -372,6 +376,6 @@
   <script type="text/javascript" src="js/jPlayer/add-on/jplayer.playlist.min.js"></script>
   <script type="text/javascript" src="js/jPlayer/demo.js"></script
 
-   
+
     </body>
 </html>
