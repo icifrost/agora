1cf13002926025094c9fff094a549e3aa269f12c
Boroma <boromamwale@gmail.com>
2016-11-19 20:12:37 +0200

    ...

---
+++ manage/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * DB configuration variables
+ */
+define("DB_HOST", "mysql9.000webhost.com");
+define("DB_USER", "a2292550_hilary");
+define("DB_PASSWORD", "smilan1899");
+define("DB_DATABASE", "a2292550_hilzdb");
+?>
--- manage/configuration.php
+++ manage/configuration.php
@@ -1,6 +1,5 @@
 <?php
 //Connect databse
-//$dbconn = mysqli_connect('mysql9.000webhost.com','a2292550_hilary','a2292550_hilzdb','smilan1899');
 define("DB_HOST", "mysql9.000webhost.com");
 define("DB_USER", "a2292550_hilary");
 define("DB_PASSWORD", "smilan1899");
