713ca8e67cb2ed722001d399d2032c9ebc5558f7
icifrost <kafwana@mwanta.com>
2016-11-21 04:55:00 +0200

    Updated Database Tables

--- agora.sql
+++ agora.sql
@@ -60,7 +60,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'admin','$2y$11$tKTQB./4PogTDebVHoYi2uzTU.TXs/5DuMRc0jgAyMQdeJGqQu.Y.',NULL,NULL,NULL,NULL,0,0,0,0,NULL,'no','0',1,NULL,NULL,NULL,1,NULL,NULL,0,0,NULL,1,0),(5,'icifrost','$2y$11$tKTQB./4PogTDebVHoYi2uzTU.TXs/5DuMRc0jgAyMQdeJGqQu.Y.','Kafwana','Wandi','kafwana@mwanta.com',1479645889,1479645889,1479644981,0,0,NULL,'no','0',1,NULL,NULL,NULL,1,NULL,NULL,0,0,NULL,1,0);
+INSERT INTO `account` VALUES (1,'admin','$2y$11$tKTQB./4PogTDebVHoYi2uzTU.TXs/5DuMRc0jgAyMQdeJGqQu.Y.',NULL,NULL,NULL,NULL,0,0,0,0,NULL,'no','0',1,NULL,NULL,NULL,1,NULL,NULL,0,0,NULL,1,0),(5,'icifrost','$2y$11$tKTQB./4PogTDebVHoYi2uzTU.TXs/5DuMRc0jgAyMQdeJGqQu.Y.','Kafwana','Wandi','kafwana@mwanta.com',1479647455,1479647455,1479645889,0,0,NULL,'no','0',1,NULL,NULL,NULL,1,NULL,NULL,0,0,NULL,1,0);
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -216,7 +216,7 @@ CREATE TABLE `forum_forums` (
   `permission_reply` int(11) DEFAULT '0',
   `last_post_time` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -225,8 +225,57 @@ CREATE TABLE `forum_forums` (
 
 LOCK TABLES `forum_forums` WRITE;
 /*!40000 ALTER TABLE `forum_forums` DISABLE KEYS */;
-INSERT INTO `forum_forums` VALUES (1,1,'How can I create a login script','Need help in creating a login script',0,0,NULL,0,NULL,-1,-1,-1,0);
+INSERT INTO `forum_forums` VALUES (1,1,'How can I create a login script','Need help in creating a login script',0,0,NULL,0,NULL,-1,-1,-1,0),(2,1,'How can I create a login script','Need help in creating a login script',0,0,NULL,0,NULL,-1,-1,-1,0);
 /*!40000 ALTER TABLE `forum_forums` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_pagecats`
+--
+
+DROP TABLE IF EXISTS `forum_pagecats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_pagecats` (
+  `page_cat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `page_cat_name` text,
+  `page_cat_order` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`page_cat_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_pagecats`
+--
+
+LOCK TABLES `forum_pagecats` WRITE;
+/*!40000 ALTER TABLE `forum_pagecats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_pagecats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_pages`
+--
+
+DROP TABLE IF EXISTS `forum_pages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_pages` (
+  `page_id` int(11) NOT NULL AUTO_INCREMENT,
+  `page_name` text,
+  `page_text` text,
+  `page_cat` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_pages`
+--
+
+LOCK TABLES `forum_pages` WRITE;
+/*!40000 ALTER TABLE `forum_pages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_pages` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -267,6 +316,207 @@ LOCK TABLES `forum_posts` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `forum_ranks`
+--
+
+DROP TABLE IF EXISTS `forum_ranks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_ranks` (
+  `rank_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rank_name` text,
+  `posts_needed` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`rank_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_ranks`
+--
+
+LOCK TABLES `forum_ranks` WRITE;
+/*!40000 ALTER TABLE `forum_ranks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_ranks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_rps`
+--
+
+DROP TABLE IF EXISTS `forum_rps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_rps` (
+  `rps_id` int(11) NOT NULL AUTO_INCREMENT,
+  `challenger` bigint(20) DEFAULT '0',
+  `challenged` bigint(20) DEFAULT '0',
+  `throw` int(11) DEFAULT '0',
+  `accept` int(11) DEFAULT '0',
+  `result` text,
+  PRIMARY KEY (`rps_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_rps`
+--
+
+LOCK TABLES `forum_rps` WRITE;
+/*!40000 ALTER TABLE `forum_rps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_rps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_tut_cats`
+--
+
+DROP TABLE IF EXISTS `forum_tut_cats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_tut_cats` (
+  `cat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cat_name` text,
+  `parent_cat` bigint(20) DEFAULT '0',
+  `num_tutorials` bigint(20) DEFAULT '0',
+  `last_added` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`cat_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_tut_cats`
+--
+
+LOCK TABLES `forum_tut_cats` WRITE;
+/*!40000 ALTER TABLE `forum_tut_cats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_tut_cats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_tut_changes`
+--
+
+DROP TABLE IF EXISTS `forum_tut_changes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_tut_changes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `edit_id` bigint(20) DEFAULT '0',
+  `email` text,
+  `title` text,
+  `author` text,
+  `cat_parent` bigint(20) DEFAULT '0',
+  `url` text,
+  `short_des` text,
+  `description` text,
+  `used_cat` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_tut_changes`
+--
+
+LOCK TABLES `forum_tut_changes` WRITE;
+/*!40000 ALTER TABLE `forum_tut_changes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_tut_changes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_tut_entries`
+--
+
+DROP TABLE IF EXISTS `forum_tut_entries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_tut_entries` (
+  `tut_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text,
+  `short_des` text,
+  `description` text,
+  `url` text,
+  `hits_out` bigint(20) DEFAULT '0',
+  `total_score` bigint(20) DEFAULT '0',
+  `total_votes` bigint(20) DEFAULT '0',
+  `avg_vote` double DEFAULT '0',
+  `rank_score` bigint(20) DEFAULT '0',
+  `validated` int(11) DEFAULT '0',
+  `cat_parent` bigint(20) DEFAULT '0',
+  `time_added` bigint(20) DEFAULT '0',
+  `date_added` text,
+  `author` text,
+  `pass_key` text,
+  `email` text,
+  PRIMARY KEY (`tut_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_tut_entries`
+--
+
+LOCK TABLES `forum_tut_entries` WRITE;
+/*!40000 ALTER TABLE `forum_tut_entries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_tut_entries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_tut_ip`
+--
+
+DROP TABLE IF EXISTS `forum_tut_ip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_tut_ip` (
+  `v_id` int(11) NOT NULL AUTO_INCREMENT,
+  `voted_for` bigint(20) DEFAULT '0',
+  `ip` text,
+  `time` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`v_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_tut_ip`
+--
+
+LOCK TABLES `forum_tut_ip` WRITE;
+/*!40000 ALTER TABLE `forum_tut_ip` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_tut_ip` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `forum_tutorial_posts`
+--
+
+DROP TABLE IF EXISTS `forum_tutorial_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum_tutorial_posts` (
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT '0',
+  `post` text,
+  `title` text,
+  `author` text,
+  `show_time` text,
+  `real_time` bigint(20) DEFAULT '0',
+  `num_replies` bigint(20) DEFAULT '0',
+  `last_poster` text,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `forum_tutorial_posts`
+--
+
+LOCK TABLES `forum_tutorial_posts` WRITE;
+/*!40000 ALTER TABLE `forum_tutorial_posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forum_tutorial_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `guestsonline`
 --
 
@@ -289,6 +539,35 @@ LOCK TABLES `guestsonline` WRITE;
 /*!40000 ALTER TABLE `guestsonline` DISABLE KEYS */;
 INSERT INTO `guestsonline` VALUES (1,'127.0.0.1',1479536866),(2,'127.0.0.1',1479536992),(3,'127.0.0.1',1479537012),(4,'127.0.0.1',1479537196),(5,'127.0.0.1',1479537781),(6,'127.0.0.1',1479538328),(7,'127.0.0.1',1479545642),(8,'127.0.0.1',1479545681),(9,'127.0.0.1',1479545794),(10,'127.0.0.1',1479545887),(11,'127.0.0.1',1479545956),(12,'127.0.0.1',1479546005),(13,'127.0.0.1',1479546066),(14,'127.0.0.1',1479546112),(15,'127.0.0.1',1479546182),(16,'127.0.0.1',1479546197),(17,'127.0.0.1',1479546278),(18,'127.0.0.1',1479546305),(19,'127.0.0.1',1479546322),(20,'127.0.0.1',1479546352),(21,'127.0.0.1',1479546658),(22,'127.0.0.1',1479546673),(23,'127.0.0.1',1479546688),(24,'127.0.0.1',1479546691),(25,'127.0.0.1',1479546704),(26,'127.0.0.1',1479546729),(27,'127.0.0.1',1479547321),(28,'127.0.0.1',1479550753),(29,'127.0.0.1',1479638990);
 /*!40000 ALTER TABLE `guestsonline` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pms`
+--
+
+DROP TABLE IF EXISTS `pms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pms` (
+  `pm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender` bigint(20) DEFAULT '0',
+  `receiver` bigint(20) DEFAULT '0',
+  `the_real_time` bigint(20) DEFAULT '0',
+  `subject` text,
+  `message` text,
+  `has_read` int(11) DEFAULT '0',
+  `var_time` text,
+  PRIMARY KEY (`pm_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pms`
+--
+
+LOCK TABLES `pms` WRITE;
+/*!40000 ALTER TABLE `pms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pms` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -316,6 +595,30 @@ INSERT INTO `staff` VALUES (1,'None');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `subscribe`
+--
+
+DROP TABLE IF EXISTS `subscribe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subscribe` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email_address` text,
+  `subscribed` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subscribe`
+--
+
+LOCK TABLES `subscribe` WRITE;
+/*!40000 ALTER TABLE `subscribe` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subscribe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping events for database 'agora'
 --
 
@@ -332,4 +635,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-20 14:46:45
+-- Dump completed on 2016-11-21  4:53:38
